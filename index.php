9305814748db751690ff90fae934801de4801d3a
Abubakar Ahmad Y <ahmaduabu@gmail.com>
2021-08-17 13:31:05 +0100

    title has been updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<title>IT Inventory System</title>
+		<title>IT Inventory System - Home page</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	</head>
 	<body>
